d766fd01c242a9cfc1726d124fea893c7bc20898
Evan Dumas <eedumas@gmail.com>
2022-08-18 14:32:26 -0700

    Change ID's from num to spelled-out, as it isn't a valid label

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-let solution = "";
+let displayNum = "";
 
 let x = null;
 let y = null;
 let temp = null;
 
 let operator = null;
+
+const num1 = document.querySelector("#one");
+const num2 = document.querySelector("#two");
+const num3 = document.querySelector("#three");
+const num4 = document.querySelector("#four");
+const num5 = document.querySelector("#five");
+const num6 = document.querySelector("#six");
+const num7 = document.querySelector("#seven");
+const num8 = document.querySelector("#eight");
+const num9 = document.querySelector("#nine");
+const num10 = document.querySelector("#zero");
+
+const addFunc = document.querySelector("#add");
+const subFunc = document.querySelector("#sub");
+const multFunc = document.querySelector("#mult");
+const divFunc = document.querySelector("#div");
+
+const clearFunc = document.querySelector("#clear");
+const equalsFunc = document.querySelector("#equals");
+
+num1.addEventListener('click', (event) => {console.log(event)});
+
 
 const operate = function(operator,a,b){
     if (operator == "add"){
@@ -40,7 +62,7 @@ const divide = function(a,b){
 
 // setting everything back to null should clear it, I hope!
 const clear = function(){
-    solution = "";
+    displayNum = "";
     x = null;
     y = null;
     temp = null;
